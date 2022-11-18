863a40e13bd32151242fa582097607b2fe266b46
DennisHsuan <boyhood515@gmail.com>
2022-11-18 11:00:45 +0800

    完成基礎模板and首頁輪播功能

--- sample/myapp/views.py
+++ sample/myapp/views.py
@@ -3,8 +3,14 @@ from django.http import HttpResponse
 from myapp.models import productModel
 
 def index(request):
+    # ORM"取"全部資料是用.all 單筆則是.get 
+    #    "放"資料是用append
     products=productModel.objects.all()#取產品資料
-    print(products) #取得成功測試
+    productlist=[]#因為只要取資料夾裡面8張照片
+    for i in range(1, 9):
+        product = productModel.objects.get(id = i)
+        productlist.append(product)
+    # print(products) #取得成功測試
 
 
     # return HttpResponse("你好")
